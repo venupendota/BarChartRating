1789cc2f67e96f2587b04820ba3d525cf1a18837
Ratheesh <ratheesh@ratheeshs-imac.local>
2018-08-30 14:07:04 +0530

    Initial Commit

---
+++ BarChartRating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarChartRating
+//
+//  Created by Ratheesh on 30/08/18.
+//  Copyright © 2018 Ratheesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
